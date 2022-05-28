64910d5a43ea6e78e63ea5ab7cf5d67b04adcf6f
sanilveeravu <mailsanilnow@yahoo.com>
2022-05-27 21:45:11 -0500

    final module update

--- static/js/app.js
+++ static/js/app.js
@@ -33,7 +33,7 @@ function handleClick(){
     if(date){
         // Apply `filter` to the table data to only keep the
         // rows where the `datetime` value matches the filter value
-        filteredData = filteredData.filter(row => datetime === date)
+        filteredData = filteredData.filter(row => row.datetime === date);  
     }
 
     // Rebuild the table using the filtered data
